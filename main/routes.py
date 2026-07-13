5cc3eb49f80a12b6a5ad66485309d1799b0c7245
Matej Koyš <matokoys66@gmail.com>
2026-07-13 12:56:45 +0200

    Fix pylint error - use os.getenv instead of os.environ.get

--- main/routes.py
+++ main/routes.py
@@ -9,7 +9,7 @@ from flask import Blueprint, request, jsonify, render_template, redirect, url_fo
 
 bp_main = Blueprint('main', __name__, template_folder='templates', static_folder='static')
 
-BASE_DIR = os.environ.get("DATA_DIR", "/tmp")
+BASE_DIR = os.getenv("DATA_DIR", "/tmp")
 DB_PATH = os.path.join(BASE_DIR, "attendance.db")
 
 ADMIN_USERNAME = "admin"
